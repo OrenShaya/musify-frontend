dc79815387ebe874c7fe4d5211ed1bbe94a9a608
Oren <xxorenxx@gmail.com>
2025-02-14 13:08:37 +0200

    WIP station detail table

--- src/cmps/StationDetailsList.jsx
+++ src/cmps/StationDetailsList.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+// import { useEffect } from 'react'
+// import { useParams } from 'react-router-dom'
+// import { useSelector } from 'react-redux'
+// import { Link } from 'react-router-dom'
 
-import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
-import { loadStation, addStationMsg } from '../store/actions/station.actions'
+// import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
+// import { loadStation, addStationMsg } from '../store/actions/station.actions'
 
 export function StationDetailsList({station}) {
-  console.log(station?.songs)  
+  // console.log(station?.songs)  
+
   
   return (
     <section className='station-details-list'>
@@ -17,6 +18,7 @@ export function StationDetailsList({station}) {
         <div className='song-name'>Title</div> 
         <div className='song-album'>Album</div> 
         <div className='song-added-date'>Date added</div> 
+        <div className='song-length'><svg data-encore-id='icon' role='img' aria-hidden='true' className='Svg-sc-ytk21e-0 dYnaPI e-9541-icon' viewBox='0 0 16 16'><path d='M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z'></path><path d='M8 3.25a.75.75 0 0 1 .75.75v3.25H11a.75.75 0 0 1 0 1.5H7.25V4A.75.75 0 0 1 8 3.25z'></path></svg></div> 
       </div>
       <hr />
       <div className='station-details-table'>
@@ -31,6 +33,12 @@ export function StationDetailsList({station}) {
             </div>
             <div className='song-album'>
               {/* {song.album} */ 'Album Name Here'}
+            </div>
+            <div>
+              {song.addedAt}
+            </div>
+            <div className='song-length'>
+              {song.lengthInSeconds}
             </div>
           </div>
         })}
