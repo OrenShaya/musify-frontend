3cc279e30e8a6915ab83b601fc8b7637dd37476d
Oren <xxorenxx@gmail.com>
2025-02-14 10:38:03 +0200

    Have some info for the playlist table

--- src/cmps/StationDetailsList.jsx
+++ src/cmps/StationDetailsList.jsx
@@ -7,7 +7,9 @@ import { Link } from 'react-router-dom'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { loadStation, addStationMsg } from '../store/actions/station.actions'
 
-export function StationDetailsList(station) {
+export function StationDetailsList({station}) {
+  console.log(station?.songs)  
+  
   return (
     <section className='station-details-list'>
       <div className='station-table'>
@@ -17,6 +19,22 @@ export function StationDetailsList(station) {
         <div className='song-added-date'>Date added</div> 
       </div>
       <hr />
+      <div className='station-details-table'>
+        {station?.songs?.length > 0 && station?.songs.map((song, idx) => {
+          return  <div className='song-row' key={song._id}>
+            <div className='song-index'>
+              {idx + 1}
+            </div>
+            <div className='song-title-artist'>
+              {song.title}
+              {song.addedBy.fullname}
+            </div>
+            <div className='song-album'>
+              {/* {song.album} */ 'Album Name Here'}
+            </div>
+          </div>
+        })}
+      </div>
     </section>
   )
 }
