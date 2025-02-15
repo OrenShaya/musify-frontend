a955b15f16ce534ffc38131d707fc23f1be6f945
Shahar Margalit <shaharma@gmail.com>
2025-02-15 14:46:08 +0200

    Style code

--- src/cmps/ScrollBtn.jsx
+++ src/cmps/ScrollBtn.jsx
@@ -11,6 +11,39 @@ export function ScrollBtn({ scrollRef, isRight = false }) {
   const btnRef = useRef()
   const isHovered = useHover(scrollRef)
 
+  const onScroll = () => {
+    if (isRight) {
+      btnRef.current.style.right = `-${scrollRef.current.scrollLeft}px`
+    } else {
+      btnRef.current.style.left = `${scrollRef.current.scrollLeft}px`
+    }
+    _toggleRender()
+  }
+
+  const onClick = () => {
+    const direction = isRight ? 1 : -1
+    scrollRef.current.scrollBy({ left: 600 * direction, behavior: 'smooth' })
+  }
+
+  // toggle render on hover
+  useEffect(() => {
+    const elScroll = scrollRef.current
+    const elBtn = btnRef.current
+
+    const clickListener = elBtn.addEventListener('click', onClick)
+
+    const scrollListener = elScroll.addEventListener('scroll', onScroll)
+
+    const resizeObserver = new ResizeObserver(_toggleRender)
+    resizeObserver.observe(document.documentElement)
+
+    return () => {
+      elBtn.removeEventListener('click', clickListener)
+      elScroll.removeEventListener('scroll', scrollListener)
+      resizeObserver.disconnect()
+    }
+  }, [isHovered])
+
   function _toggleRender() {
     const elScroll = scrollRef.current
     const elBtn = btnRef.current
@@ -35,40 +68,6 @@ export function ScrollBtn({ scrollRef, isRight = false }) {
     if (display) elBtn.classList.add('display')
     else elBtn.classList.remove('display')
   }
-
-  // toggle render on hover
-  useEffect(() => {
-    const elScroll = scrollRef.current
-    const elBtn = btnRef.current
-
-    // Scroll the station list when the button is clicked
-    const clickListener = elBtn.addEventListener('click', () => {
-      const direction = isRight ? 1 : -1
-      elScroll.scrollBy({ left: 600 * direction, behavior: 'smooth' })
-    })
-
-    const scrollListener = elScroll.addEventListener('scroll', () => {
-      // Should move button left?
-      if (isRight) {
-        elBtn.style.right = `-${elScroll.scrollLeft}px`
-      } else {
-        elBtn.style.left = `${elScroll.scrollLeft}px`
-      }
-      _toggleRender()
-    })
-
-    const resizeObserver = new ResizeObserver(() => {
-      _toggleRender()
-    })
-    resizeObserver.observe(document.documentElement)
-
-    return () => {
-      elBtn.removeEventListener('click', clickListener)
-      elScroll.removeEventListener('scroll', scrollListener)
-      resizeObserver.disconnect()
-    }
-  }, [isHovered])
-
   const btnClassName = isRight ? 'btn-scroll-right' : 'btn-scroll-left'
   const scrollUrl = isRight ? scrollRightUrl : scrollLeftUrl
   return (
