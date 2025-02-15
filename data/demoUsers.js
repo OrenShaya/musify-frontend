c72f326ce783e79fb97f1fdc210db5c197ba8708
Dor-J <Mr.DorJosef@gmail.com>
2025-02-15 04:05:22 +0200

    Add demo data

---
+++ data/demoUsers.js
@@ -0,0 +1,352 @@
+// export const demoUsers = [
+//   {
+//     username: 'admin',
+//     password: 'admin',
+//     fullname: 'admin',
+//     isAdmin: true,
+//     likedStationIds: [],
+//     likedSongIds: [],
+//     imgUrl:
+//       'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+//     _id: '3S4c4',
+//   },
+//   {
+//     username: 'kiki',
+//     password: 'kiki',
+//     fullname: 'kiki',
+//     isAdmin: false,
+//     likedStationIds: [],
+//     likedSongIds: [],
+//     imgUrl:
+//       'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+//     _id: '3T5yg',
+//   },
+//   {
+//     username: 'muki',
+//     password: 'muki',
+//     fullname: 'muki',
+//     isAdmin: false,
+//     likedStationIds: [],
+//     likedSongIds: [],
+//     imgUrl:
+//       'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+//     _id: 'NuBkh',
+//   },
+//   {
+//     username: 'shuki',
+//     password: 'shuki',
+//     fullname: 'shuki',
+//     isAdmin: false,
+//     likedStationIds: [],
+//     likedSongIds: [],
+//     imgUrl:
+//       'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+//     _id: 'Oeiwy',
+//   },
+//   {
+//     username: 'baba',
+//     password: 'baba',
+//     fullname: 'baba',
+//     isAdmin: false,
+//     likedStationIds: [],
+//     likedSongIds: [],
+//     imgUrl:
+//       'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+//     _id: '7N2Ai',
+//   },
+//   {
+//     username: 'puki',
+//     password: 'puki',
+//     fullname: 'Puki Ben David',
+//     isAdmin: false,
+//     likedStationIds: [],
+//     likedSongIds: [],
+//     imgUrl:
+//       'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+//     _id: 'u101',
+//   },
+// ]
+export const demoUsers = [
+  {
+    username: 'admin',
+    password: 'admin',
+    fullname: 'admin',
+    isAdmin: true,
+    likedStationIds: [],
+    likedSongIds: [],
+    imgUrl:
+      'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+    _id: '3S4c4',
+  },
+  {
+    username: 'kiki',
+    password: 'kiki',
+    fullname: 'kiki',
+    isAdmin: false,
+    likedStationIds: [],
+    likedSongIds: [],
+    imgUrl:
+      'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+    _id: '3T5yg',
+  },
+  {
+    username: 'muki',
+    password: 'muki',
+    fullname: 'muki',
+    isAdmin: false,
+    likedStationIds: [],
+    likedSongIds: [],
+    imgUrl:
+      'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+    _id: 'NuBkh',
+  },
+  {
+    username: 'shuki',
+    password: 'shuki',
+    fullname: 'shuki',
+    isAdmin: false,
+    likedStationIds: [],
+    likedSongIds: [],
+    imgUrl:
+      'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+    _id: 'Oeiwy',
+  },
+  {
+    username: 'baba',
+    password: 'baba',
+    fullname: 'baba',
+    isAdmin: false,
+    likedStationIds: [],
+    likedSongIds: [],
+    imgUrl:
+      'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+    _id: '7N2Ai',
+  },
+  {
+    username: 'puki',
+    password: 'puki',
+    fullname: 'Puki Ben David',
+    isAdmin: false,
+    likedStationIds: [],
+    likedSongIds: [],
+    imgUrl:
+      'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+    _id: 'u101',
+  },
+  {
+    _id: 'UC7-YMmnc0ppcWmio8t1WdcA',
+    username: 'System Of A Down',
+    fullname: 'System Of A Down',
+    password: 'System Of A Downpass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_m6xYq7ErRAA9YY3XG3NLAOZJRfV67Edf-dV9qZD2T-Ak4=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1131042368000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCNifdat21zAF8ux2AcQBgYg',
+    username: 'Rodolfo Carmacio',
+    fullname: 'Rodolfo Carmacio',
+    password: 'Rodolfo Carmaciopass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_m8nVVONNJpq0VOCk2ozCtz9iVXPqiKdXl9qwn4gr42fWM=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1464196588000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UC07Kxew-cMIaykMOkzqHtBQ',
+    username: 'LadyGagaVEVO',
+    fullname: 'LadyGagaVEVO',
+    password: 'LadyGagaVEVOpass',
+    imgUrl:
+      'https://yt3.ggpht.com/PhFThrqp58joMS9FOd7I2jPZ1TepvCKDsdnuwmFXPl-Tq0kZjw3GNtCNtsxt3dgH8bqW7RdK=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1242106108000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+  {
+    _id: 'UCpvEzypPseM9j8XaEA6cbLg',
+    username: 'Le Sémaphore',
+    fullname: 'Le Sémaphore',
+    password: 'Le Sémaphorepass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_nbNP4C904JTZDyi2HWE7ekHFoCZh2EzYHsaxxnheWJBLE=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1390582313000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCetWI4ePnNOLGyM6kNuYhCg',
+    username: 'RadGuitarPro',
+    fullname: 'RadGuitarPro',
+    password: 'RadGuitarPropass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_kR3FYFTk2BxlxOAL9frFEuCSqi9fXLV0mSZ_wmPFM-FA=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1306158905000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+  {
+    _id: 'UCdTNOsPzOJzi3fXDObomEaQ',
+    username: 'Hunter Crow',
+    fullname: 'Hunter Crow',
+    password: 'Hunter Crowpass',
+    imgUrl:
+      'https://yt3.ggpht.com/6yy6VZ0AnHnPnnxZtTG3ZPirWg_TwgzuJsR8Lo759rWIgGhHMUuwVufs-M_kJ-MARiaBxfv3cA=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1322740795000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCjIILWUX89AHJmDpFCZ2-og',
+    username: 'Seven Hip-Hop',
+    fullname: 'Seven Hip-Hop',
+    password: 'Seven Hip-Hoppass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_mMIBaUXk5Vy0xMiC36_QAolSCQg_9fCrgASqDdlJT2c2s=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1325020829000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+  {
+    _id: 'UC5GOr7CavTbxAf18CKjukdg',
+    username: 'Radio Golcak',
+    fullname: 'Radio Golcak',
+    password: 'Radio Golcakpass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_l7PqzcYrf354E8jS_WG589jUsRcX5vmXU0Qyo-SlfmNO8=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1424821139000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCxMAbVFmxKUVGAll0WVGpFw',
+    username: 'Cardi B',
+    fullname: 'Cardi B',
+    password: 'Cardi Bpass',
+    imgUrl:
+      'https://yt3.ggpht.com/TPBNdSphJ6uErZqo4lRTOC_L-RtI4iWjI6tj5mokbbZ46f_pxE8NToKvXQfoNSDa8J6dsUUJ=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1480603447000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UC3jOd7GUMhpgJRBhiLzuLsg',
+    username: 'Nicki Minaj',
+    fullname: 'Nicki Minaj',
+    password: 'Nicki Minajpass',
+    imgUrl:
+      'https://yt3.ggpht.com/50YeejQslEmP9_pNJHm3SGYRHRuVMuGafgsyrm2AjRNOE8P0bcIsDMA371xebxsd2zMQedm0TA=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1377703164000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+  {
+    _id: 'UC3jOd7GUMhpgJRBhiLzuLsg',
+    username: 'Nicki Minaj',
+    fullname: 'Nicki Minaj',
+    password: 'Nicki Minajpass',
+    imgUrl:
+      'https://yt3.ggpht.com/50YeejQslEmP9_pNJHm3SGYRHRuVMuGafgsyrm2AjRNOE8P0bcIsDMA371xebxsd2zMQedm0TA=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1377703164000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCqfmagAXUrUTHpJldTPRseg',
+    username: 'ScorpionsVEVO',
+    fullname: 'ScorpionsVEVO',
+    password: 'ScorpionsVEVOpass',
+    imgUrl:
+      'https://yt3.ggpht.com/v1Cr6_lKxC5ERii2furlU5CyjJ4MFcWjy9ev0V5f2qJiT4VMdOsWWDJ6NYtbRCOGZ6CN6IqcK28=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1242106157000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCTfQnoSX4wqmQi0t-O-h_FQ',
+    username: 'MusicVEVO',
+    fullname: 'MusicVEVO',
+    password: 'MusicVEVOpass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_m-wvAwKyNneCMdoErnw329VPrjfATdBmyawkiFTmB0Tmcb0mbPWFH98RzurWL1m0u29A=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1676773192518,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UC5HPMnG14ZkcRfTS2eLGEog',
+    username: "O'Shea Jackson",
+    fullname: "O'Shea Jackson",
+    password: "O'Shea Jacksonpass",
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_lO0Dc1xL3e5hX7riLsQGKxW8yIA7yF_SVhSjzLk_-2KfU=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1295570089000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCqnXx4LZbWT2QGw36W8fnNA',
+    username: 'Tom Inoccic',
+    fullname: 'Tom Inoccic',
+    password: 'Tom Inoccicpass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_krdUKXHh_o8LTiPuLJE35bw4ibTclurp-hEJkmH1dv1UI=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1364949546000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UCPvkTCeGc0U7KUL5QVcKF-w',
+    username: 'Trancentral',
+    fullname: 'Trancentral',
+    password: 'Trancentralpass',
+    imgUrl:
+      'https://yt3.ggpht.com/ytc/AIdro_lweS3sM5_ixQuek_95HMezADC0a-0E6M_dJsF_jUNfty4=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1404387277000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+
+  {
+    _id: 'UC_kRDKYrUlrbtrSiyu5Tflg',
+    username: 'Daft Punk',
+    fullname: 'Daft Punk',
+    password: 'Daft Punkpass',
+    imgUrl:
+      'https://yt3.ggpht.com/TPq4rBZZrU9AyYVuAiG6ndO61ZZ-8kckaXNI_k6TqfabWhCnhx0CY9-ilMFaE2Xn9yCt9s8b=s800-c-k-c0x00ffffff-no-rj',
+    createdAt: 1189430195000,
+    likedStationIds: [],
+    likedSongIds: [],
+    isAdmin: false,
+  },
+]
