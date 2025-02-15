67432fe3e4ab15f0205102c6bdcb66db5baad3c2
Dor-J <Mr.DorJosef@gmail.com>
2025-02-15 13:52:07 +0200

    Add audio player service as singelton using class

---
+++ src/services/player/audioPlayer.service.js
@@ -0,0 +1,82 @@
+/**
+ * For the audio player, I used a singelton architecure
+ * This singelton wraps iframe player functions
+ * I used classes and getters and setters, Java style
+ */
+
+class AudioPlayer {
+  constructor() {
+    // instance check
+    if (AudioPlayer.instance) {
+      return AudioPlayer.instance
+    }
+
+    //singelton audio element
+    this.audio = new Audio()
+    this.audio.volume = 1
+
+    // listen
+    this.audio.addEventListener('timeupdate', () => {
+      window.parent.postMessage({
+        type: 'timeupdate',
+        currentTime: this.audio.currentTime,
+        duration: this.audio.duration,
+      })
+    })
+
+    // cacheing
+    AudioPlayer.instance = this
+  }
+
+  // getIsntance
+  static getInstance() {
+    if (!AudioPlayer.instance) {
+      AudioPlayer.instance = new AudioPlayer()
+    }
+    return AudioPlayer.instance
+  }
+
+  // methods
+
+  play() {
+    return this.audio.play()
+  }
+
+  pause() {
+    return this.audio.pause()
+  }
+
+  // currentTime - getter setter
+  get currentTime() {
+    return this.audio.currentTime
+  }
+
+  set currentTime(time) {
+    this.audio.currentTime = time
+  }
+
+  // duration
+  get duration() {
+    return this.audio.duration
+  }
+
+  // volume - getter setter
+  get volume() {
+    return this.audio.volume
+  }
+
+  set volume(vol) {
+    this.audio.volume = vol
+  }
+
+  //important! src setter
+  setSource(src) {
+    this.audio.src = src
+  }
+}
+
+// instance
+const audioPlayer = AudioPlayer.getInstance()
+
+//export instance to a module
+export default audioPlayer
