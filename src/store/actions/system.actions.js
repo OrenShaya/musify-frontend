1e368839bafc6df5af617ffd44aed08c78124555
Dor-J <Mr.DorJosef@gmail.com>
2025-02-15 12:49:37 +0200

    Add state for currently playing song

--- src/store/actions/system.actions.js
+++
@@ -1,6 +0,0 @@
-import { store } from '../store'
-import { TOGGLE_IS_PLAYING } from '../reducers/system.reducer'
-
-export function toggleIsPlaying() {
-  store.dispatch({ type: TOGGLE_IS_PLAYING })
-}
