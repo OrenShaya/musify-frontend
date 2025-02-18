4812c7c9dd881f2a9a8592028c2c7410a4c52fc9
Dor-J <Mr.DorJosef@gmail.com>
2025-02-18 13:34:09 +0200

    Add condition to display table header

--- src/cmps/StationDetailsList.jsx
+++ src/cmps/StationDetailsList.jsx
@@ -32,24 +32,26 @@ export function StationDetailsList({ station }) {
 
   return (
     <section className='station-details-list'>
-      <div className='station-table'>
-        <div className='song-number'>#</div>
-        <div className='song-name'>Title</div>
-        <div className='song-album-table-head'>Album</div>
-        <div className='song-added-date'>Date added</div>
-        <div className='song-length-icon'>
-          <svg
-            data-encore-id='icon'
-            role='img'
-            aria-hidden='true'
-            className='Svg-sc-ytk21e-0 dYnaPI e-9541-icon'
-            viewBox='0 0 16 16'
-          >
-            <path d='M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z'></path>
-            <path d='M8 3.25a.75.75 0 0 1 .75.75v3.25H11a.75.75 0 0 1 0 1.5H7.25V4A.75.75 0 0 1 8 3.25z'></path>
-          </svg>
+      {station?.songs?.length > 0 && (
+        <div className='station-table'>
+          <div className='song-number'>#</div>
+          <div className='song-name'>Title</div>
+          <div className='song-album-table-head'>Album</div>
+          <div className='song-added-date'>Date added</div>
+          <div className='song-length-icon'>
+            <svg
+              data-encore-id='icon'
+              role='img'
+              aria-hidden='true'
+              className='Svg-sc-ytk21e-0 dYnaPI e-9541-icon'
+              viewBox='0 0 16 16'
+            >
+              <path d='M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z'></path>
+              <path d='M8 3.25a.75.75 0 0 1 .75.75v3.25H11a.75.75 0 0 1 0 1.5H7.25V4A.75.75 0 0 1 8 3.25z'></path>
+            </svg>
+          </div>
         </div>
-      </div>
+      )}
       <hr />
       <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId='droppable'>
