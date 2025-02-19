a38bbf1142d228c17c5b7c5d1bed1469124d7244
Shahar Margalit <shaharma@gmail.com>
2025-02-20 01:26:46 +0200

    Remove overlap

--- src/store/store.js
+++ src/store/store.js
@@ -4,14 +4,12 @@ import {
   legacy_createStore as createStore,
 } from 'redux'
 import { stationReducer } from './reducers/station.reducer'
-import { stationGroupReducer } from './reducers/stationGroup.reducer'
 import { userReducer } from './reducers/user.reducer'
 import { playerReducer } from './reducers/player.reducer'
 import { systemReducer } from './reducers/system.reducer'
 
 const rootReducer = combineReducers({
   stationModule: stationReducer,
-  stationGroupModule: stationGroupReducer,
   userModule: userReducer,
   systemModule: systemReducer,
   playerModule: playerReducer,
