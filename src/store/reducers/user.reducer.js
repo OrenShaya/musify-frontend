be02e28e5673160ec3c6911e3c134b6ad8ff6a0d
Shahar Margalit <shaharma@gmail.com>
2025-02-20 01:23:54 +0200

    Clean up

--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -6,7 +6,6 @@ export const REMOVE_USER = 'REMOVE_USER'
 export const SET_USERS = 'SET_USERS'
 
 const initialState = {
-  count: 10,
   user: userService.getLoggedinUser() || null,
   users: [],
   watchedUser: null,
