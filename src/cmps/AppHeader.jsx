aa027d918f65ad68f1aa2d208f08a4abdf677a4c
Shahar Margalit <shaharma@gmail.com>
2025-02-20 11:05:09 +0200

    Improve readability

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,7 +1,7 @@
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useLocation, useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+import { showErrorMsg } from '../services/event-bus.service'
 import { logout } from '../store/actions/user.actions'
 
 import profileUrl from '../assets/img/profile-avatar.svg'
@@ -16,8 +16,6 @@ import { useRef, useState } from 'react'
 export function AppHeader() {
   const user = useSelector((storeState) => storeState.userModule.user)
   const navigate = useNavigate()
-  const location = useLocation()
-  const searchbarRef = useRef(null)
   const [showUserMenu, setShowUserMenu] = useState(false)
 
   function onUserClick(ev) {
@@ -34,58 +32,20 @@ export function AppHeader() {
     }
   }
 
-  function resetSearchBar() {
-    if (searchbarRef.current) {
-      searchbarRef.current.value = ''
-    }
-  }
-  const homeIconUrl = location.pathname === '/' ? homeFilledUrl : homeUrl
-  const browserIconUrl =
-    location.pathname === '/explore' ? browseFilledUrl : browseUrl
-
   return (
     <header className='app-header full'>
       <nav>
+        {/* Logo */}
         <NavLink className='header-logo-link'>
           <img className='logo' src={spotifyLogoUrl} alt='spotify logo' />
         </NavLink>
 
+        {/* Middle link group */}
         <div className='nav-links'>
-          {/* <NavLink to='about'>About</NavLink> */}
-
-          <NavLink to='/' className='home-btn'>
-            <img className='header-home-icon' src={homeIconUrl} />
-          </NavLink>
-
-          <div className='search-container'>
-            <span className='search-icon'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                x='0px'
-                y='0px'
-                width='29'
-                height='29'
-                viewBox='0 0 30 30'
-              >
-                <path d='M 13 3 C 7.4886661 3 3 7.4886661 3 13 C 3 18.511334 7.4886661 23 13 23 C 15.396652 23 17.59741 22.148942 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148942 17.59741 23 15.396652 23 13 C 23 7.4886661 18.511334 3 13 3 z M 13 5 C 17.430666 5 21 8.5693339 21 13 C 21 17.430666 17.430666 21 13 21 C 8.5693339 21 5 17.430666 5 13 C 5 8.5693339 8.5693339 5 13 5 z'></path>
-              </svg>
-            </span>
-            <input
-              className='search-bar'
-              type='search'
-              ref={searchbarRef}
-              placeholder='What do you want to play?'
-            />
-
-            <NavLink to='/explore' className='browser-btn'>
-              <img className='browser-icon' src={browserIconUrl} />
-            </NavLink>
-            <button type='reset' className='reset-btn' onClick={resetSearchBar}>
-              <img className='reset-icon' src={resetUrl} />
-            </button>
-          </div>
+          <NavlinkGroup />
         </div>
 
+        {/* Profile \ Log in */}
         <div className='profile-section'>
           <Profile
             user={user}
@@ -119,6 +79,56 @@ const Profile = ({ user, showUserMenu, onLogout, onUserClick }) => {
         <div className='user-info btn-header-profile'>
           {user.imgUrl && <img src={user.imgUrl} onClick={onUserClick} />}
         </div>
+      </div>
+    </>
+  )
+}
+
+const NavlinkGroup = () => {
+  const location = useLocation()
+
+  const searchbarRef = useRef(null)
+
+  const homeIconUrl = location.pathname === '/' ? homeFilledUrl : homeUrl
+  const browserIconUrl =
+    location.pathname === '/explore' ? browseFilledUrl : browseUrl
+
+  function resetSearchBar() {
+    if (searchbarRef.current) {
+      searchbarRef.current.value = ''
+    }
+  }
+
+  return (
+    <>
+      <NavLink to='/' className='home-btn'>
+        <img className='header-home-icon' src={homeIconUrl} />
+      </NavLink>
+      <div className='search-container'>
+        <span className='search-icon'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            x='0px'
+            y='0px'
+            width='29'
+            height='29'
+            viewBox='0 0 30 30'
+          >
+            <path d='M 13 3 C 7.4886661 3 3 7.4886661 3 13 C 3 18.511334 7.4886661 23 13 23 C 15.396652 23 17.59741 22.148942 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148942 17.59741 23 15.396652 23 13 C 23 7.4886661 18.511334 3 13 3 z M 13 5 C 17.430666 5 21 8.5693339 21 13 C 21 17.430666 17.430666 21 13 21 C 8.5693339 21 5 17.430666 5 13 C 5 8.5693339 8.5693339 5 13 5 z'></path>
+          </svg>
+        </span>
+        <input
+          className='search-bar'
+          type='search'
+          ref={searchbarRef}
+          placeholder='What do you want to play?'
+        />
+        <NavLink to='/explore' className='browser-btn'>
+          <img className='browser-icon' src={browserIconUrl} />
+        </NavLink>
+        <button type='reset' className='reset-btn' onClick={resetSearchBar}>
+          <img className='reset-icon' src={resetUrl} />
+        </button>
       </div>
     </>
   )
