b727206fc865b1ba7781d47d8111cce5867e678f
Dor-J <Mr.DorJosef@gmail.com>
2025-02-20 03:01:33 +0200

    Add props func to set filtered term

--- src/cmps/StationExploreArticle.jsx
+++ src/cmps/StationExploreArticle.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-export function StationExploreArticle({ tagObj }) {
+export function StationExploreArticle({ tagObj, setFilteredTerm }) {
   const { tagName, bgColor, imgUrl } = tagObj
   return (
     <div
       className='station-explore-article'
       key={tagName}
       style={{ backgroundColor: bgColor }}
+      onClick={() => setFilteredTerm(tagName)}
     >
       <div className='genre-header'>
         <span>{tagName}</span>
--- src/cmps/StationExploreGrid.jsx
+++ src/cmps/StationExploreGrid.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { StationExploreArticle } from './StationExploreArticle'
 
-export function StationExploreGrid({ tagsObj }) {
+export function StationExploreGrid({ tagsObj, setFilteredTerm }) {
   return (
     <section className='station-explore-grid'>
       <div className='explore-grid-container'>
@@ -9,7 +9,11 @@ export function StationExploreGrid({ tagsObj }) {
           <h2>Browse all</h2>
         </div>
         {tagsObj.map((tagObj) => (
-          <StationExploreArticle key={tagObj._id} tagObj={tagObj} />
+          <StationExploreArticle
+            key={tagObj._id}
+            tagObj={tagObj}
+            setFilteredTerm={setFilteredTerm}
+          />
         ))}
       </div>
     </section>
