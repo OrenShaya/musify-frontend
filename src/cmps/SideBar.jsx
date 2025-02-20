937f96a955a3c581d3285eefa16a26556b27871a
Dan Tayari <kisa@tayari.co.il>
2025-02-20 19:11:43 +0200

    Bug fixed

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { addStation } from '../store/actions/station.actions'
 import { SideBarStationPreview } from './SideBarStationPreview'
 import stationDefaultUrl from '../assets/icons/station-default-sidebar-img.svg'
@@ -10,7 +10,7 @@ import { useSelector } from 'react-redux'
 export function SideBar() {
   const addStationRef = useRef()
   const navigate = useNavigate()
-  const stations = useSelector((s) => s.stationModule.stations)
+  const [stations, setStations] = useState([])
   const [filterTerm, setFilterTerm] = useState()
   const PLAYLIST_CONTAINER = useRef(null)
   const [isSearchExpanded, setIsSearchExpanded] = useState(false)
@@ -37,7 +37,7 @@ export function SideBar() {
     return () => {
       elAddStation.removeEventListener('click', handleClick)
     }
-  }, [filterTerm])
+  }, [filterTerm, stations])
 
   function toggleHiddenClass() {
     PLAYLIST_CONTAINER.current.classList.toggle('hidden')
@@ -122,7 +122,7 @@ export function SideBar() {
             </svg>
             Create a new playlist
           </button>
-          <button>
+          {/* <button>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               data-encore-id='icon'
@@ -136,7 +136,7 @@ export function SideBar() {
               <path d='M1.75 1A1.75 1.75 0 0 0 0 2.75v11.5C0 15.216.784 16 1.75 16h12.5A1.75 1.75 0 0 0 16 14.25v-9.5A1.75 1.75 0 0 0 14.25 3H7.82l-.65-1.125A1.75 1.75 0 0 0 5.655 1H1.75zM1.5 2.75a.25.25 0 0 1 .25-.25h3.905a.25.25 0 0 1 .216.125L6.954 4.5h7.296a.25.25 0 0 1 .25.25v9.5a.25.25 0 0 1-.25.25H1.75a.25.25 0 0 1-.25-.25V2.75z' />
             </svg>
             Create a random playlist
-          </button>
+          </button> */}
         </ul>
       </div>
 
