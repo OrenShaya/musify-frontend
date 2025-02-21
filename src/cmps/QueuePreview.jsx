a8f965d12a68c7e345b120805fe06bc704e77c88
Dor-J <Mr.DorJosef@gmail.com>
2025-02-22 01:44:10 +0200

    Refactor queue preview

--- src/cmps/QueuePreview.jsx
+++ src/cmps/QueuePreview.jsx
@@ -1,21 +1,31 @@
 /* eslint-disable no-unused-vars */
+
+import { useSelector } from 'react-redux'
+import { setCurrentlyPlaying } from '../store/actions/player.actions'
+
 /* eslint-disable react/prop-types */
 export function QueuePreview({ currSong }) {
-  const { title, url, addedBy, lengthInSeconds } = currSong
-  const songId = currSong.yt_id
-  const songImgUrl = currSong.imgUrl
-  const { fullname } = addedBy
-  const artistId = addedBy.yt_id
-  const artistImgUrl = addedBy.imgUrl
+  const station = useSelector((s) => s.stationModule.station)
+
+  const songId = currSong?.yt_id
+  const songImgUrl = currSong?.imgUrl
+  const artistName = currSong?.addedBy?.fullname
+  const songName = currSong?.title
+
   return (
     <section className='queue-preview'>
-      <article className='queue-preview-article' key={songId}>
-        <div className='image-container'>
-          <img src={songImgUrl} alt={`img of ${title}`} />{' '}
-        </div>
-        <h3 className='queue-song-title'>{title}</h3>
-        <span className='queue-song-artist'>{fullname}</span>
-      </article>
+      {currSong && (
+        <article className='queue-preview-article' key={songId}>
+          <div
+            className='image-container'
+            onClick={setCurrentlyPlaying(station, songId)}
+          >
+            <img src={songImgUrl} alt={`img of ${songName}`} />{' '}
+          </div>
+          <h3 className='queue-song-title'>{songName}</h3>
+          <span className='queue-song-artist'>{artistName}</span>
+        </article>
+      )}
     </section>
   )
 }
