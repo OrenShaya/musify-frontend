6996867fc34fe21d0607a23592c5551d1d4521b0
Shahar Margalit <shaharma@gmail.com>
2025-02-22 20:47:59 +0200

    Add navigate to mobile footer

--- src/cmps/AppFooterMobile.jsx
+++ src/cmps/AppFooterMobile.jsx
@@ -1,6 +1,11 @@
 import homeUrl from '../assets/img/home.svg'
+import homeFilledUrl from '../assets/img/home-filled.svg'
 import searchUrl from '../assets/img/search.svg'
+import searchFilledUrl from '../assets/img/search-filled.svg'
 import libraryUrl from '../assets/img/library.svg'
+import libraryFilledUrl from '../assets/img/library-filled.svg'
+import { useNavigate } from 'react-router'
+import { useState } from 'react'
 
 export function AppFooterMobile() {
   return (
@@ -13,17 +18,37 @@ export function AppFooterMobile() {
 }
 
 function MobileNavigator() {
+  const navigate = useNavigate()
+  const [page, setPage] = useState('/')
+
   const icons = [
-    { name: 'Home', url: homeUrl },
-    { name: 'Search', url: searchUrl },
-    { name: 'Your Library', url: libraryUrl },
+    { name: 'Home', url: page === '/' ? homeFilledUrl : homeUrl, page: '/' },
+    {
+      name: 'Search',
+      url: page === '/search' ? searchFilledUrl : searchUrl,
+      page: '/search',
+    },
+    {
+      name: 'Your Library',
+      url: page === '/library' ? libraryFilledUrl : libraryUrl,
+      page: '/library',
+    },
   ]
+
+  const onNavigate = (icon) => {
+    setPage(icon.page)
+    navigate(icon.page)
+  }
 
   return (
     <section>
       <ul className='mobile-navigator-list'>
         {icons.map((icon, i) => (
-          <li key={i} className='mobile-navigator-item'>
+          <li
+            key={i}
+            className='mobile-navigator-item'
+            onClick={() => onNavigate(icon)}
+          >
             <div className='icon'>
               <img src={icon.url} alt='' />
             </div>
