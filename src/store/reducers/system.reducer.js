eeecdc45b21d2c188e9be591290f2c79d2a4efc0
Dor-J <Mr.DorJosef@gmail.com>
2025-02-22 22:30:01 +0200

    Add reducer for searchTerm in system

--- src/store/reducers/system.reducer.js
+++ src/store/reducers/system.reducer.js
@@ -1,8 +1,10 @@
 export const LOADING_START = 'LOADING_START'
 export const LOADING_DONE = 'LOADING_DONE'
+export const SET_SEARCH_TERM = 'SET_SEARCH_TERM'
 
 const initialState = {
   isLoading: false,
+  searchTerm: '',
 }
 
 export function systemReducer(state = initialState, action = {}) {
@@ -12,6 +14,9 @@ export function systemReducer(state = initialState, action = {}) {
 
     case LOADING_DONE:
       return { ...state, isLoading: false }
+
+    case SET_SEARCH_TERM:
+      return { ...state, searchTerm: action.newSearchTerm }
 
     default:
       return state
