a9630fa4a5260b471dcd81fefbefdd795cc3a119
Shahar Margalit <shaharma@gmail.com>
2025-02-23 12:23:54 +0200

    Extract Like button

---
+++ src/cmps/LikeButton.jsx
@@ -0,0 +1,22 @@
+import greenTickUrl from '../assets/icons/green-tick.svg'
+import addLikedSongUrl from '../assets/img/add-liked-song.svg'
+
+export function LikeButton({ song, isLiked, onLikeSong }) {
+  const getClasses = () => {
+    const classes = ['add-to-liked']
+    if (!isLiked) classes.push('add')
+
+    return classes.join(' ')
+  }
+
+  const imgUrl = isLiked ? greenTickUrl : addLikedSongUrl
+
+  return (
+    <button
+      className='clean-btn'
+      onClick={() => onLikeSong(song?.yt_id, !isLiked)}
+    >
+      <img className={getClasses()} src={imgUrl} />
+    </button>
+  )
+}
